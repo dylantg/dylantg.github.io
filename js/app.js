346226daeecaa9a87e14181abefd8a523370531e
Dylan Graham <dylan.t.graham@gmail.com>
2017-10-26 18:52:02 -0400

    Migratory commit

---
+++ js/app.js
@@ -0,0 +1,265 @@
+$(function() {
+
+  var size = 6;
+  var win = 4;
+  var current_player = 1;
+  // Red is +1, Yellow is -1
+  var board = get_empty_board(size); //[[], [], [], [], []];
+  // var board = [[1], [-1], [1, -1], [], []];
+
+  // Listen on button clicks
+  $('#flip_left').click(flip_left);
+  $('#flip_right').click(flip_right);
+  $('#flip_over').click(flip_over);
+
+  // Listen to column buttons
+  $('#plus_0').click(add_to_col_0);
+  $('#plus_1').click(add_to_col_1);
+  $('#plus_2').click(add_to_col_2);
+  $('#plus_3').click(add_to_col_3);
+  $('#plus_4').click(add_to_col_4);
+  $('#plus_5').click(add_to_col_5);
+
+  $('#minus_0').click(subtract_from_col_0);
+  $('#minus_1').click(subtract_from_col_1);
+  $('#minus_2').click(subtract_from_col_2);
+  $('#minus_3').click(subtract_from_col_3);
+  $('#minus_4').click(subtract_from_col_4);
+  $('#minus_5').click(subtract_from_col_5);
+
+  function add_to_col_0() {
+    plus(0, current_player);
+  }
+
+  function add_to_col_1() {
+    plus(1, current_player);
+  }
+
+  function add_to_col_2() {
+    plus(2, current_player);
+  }
+
+  function add_to_col_3() {
+    plus(3, current_player);
+  }
+
+  function add_to_col_4() {
+    plus(4, current_player);
+  }
+
+  function add_to_col_5() {
+    plus(5, current_player);
+  }
+
+  function subtract_from_col_0() {
+    minus(0);
+  }
+
+  function subtract_from_col_1() {
+    minus(1);
+  }
+
+  function subtract_from_col_2() {
+    minus(2);
+  }
+
+  function subtract_from_col_3() {
+    minus(3);
+  }
+
+  function subtract_from_col_4() {
+    minus(4);
+  }
+
+  function subtract_from_col_5() {
+    minus(5);
+  }
+
+  // Listen on column clicks (if neccessary)
+
+
+
+  // Add function: Adds play to column if available
+  function plus(col, play) {
+    console.log("Plus: " + col + ":" + play);
+    if (board[col].length < size) {
+      board[col].push(play);
+      end_turn();
+    }
+  }
+
+  // Minus function
+  function minus(row) {
+    console.log("Minus: " + row);
+    if (board[row].length > 0) {
+      board[row].shift();
+      end_turn();      
+    }
+  }
+
+// FLIPS ////////////////////////////////////////////////////////
+  // Flip left
+  function flip_left() {
+    var temp = get_empty_board(size);
+    // For every row
+    for (j = size - 1; j >= 0; j--) {
+      // For every column
+      for (i = 0; i < size; i++) {
+        var value = board[i][j];
+        if (value != null) {
+          temp[size - 1  - j].push(value);
+        }
+      }
+    }
+    board = temp;
+    end_turn();
+  }
+
+  // Flip right
+  function flip_right() {
+    var temp = get_empty_board(size);
+    // For every row
+    for (j = 0; j < size; j++) {
+      // For every column
+      for (i = size - 1; i >= 0; i--) {
+        var value = board[i][j];
+        if (value != null) {
+          temp[j].push(value);
+        }
+      }
+    }
+    board = temp;
+    end_turn();
+  }
+
+  // Flip over
+  function flip_over() {
+    for (i = 0; i < size; i++) {
+      board[i].reverse();
+    }
+    end_turn();
+  }
+
+// WIN CHECKS ////////////////////////////////////////////////////////
+  // Check for win
+  function check_columns() {
+    r_win = 0;
+    y_win = 0;
+    for (i = 0; i < size; i++) {
+      col_size = board[i].length;
+      if (col_size >= win){
+        for (j = 0; j < col_size - win + 1; j++) {
+          sum = 0;
+          for (k = 0; k < win; k++) {
+            space = board[i][j + k];
+            if (space != null) {
+              sum += space;
+            }
+          }
+          if (sum === win) {
+            r_win++;
+          } else if (sum === (-1 * win)) {
+            y_win++;
+          }
+        }
+      }
+    }
+    return  {'r':r_win, 'y':y_win}
+  }
+
+  function check_rows() {
+    r_win = 0;
+    y_win = 0;
+    for (j = 0; j < size; j++) {
+      for (i = 0; i < size; i++) {
+        sum = 0;
+        for (k = 0; k < win; k++) {
+          if (i + k <= size -1) {
+            space = board[i + k][j];
+            if (space != null) {
+              sum += space;
+            }            
+          }
+        }
+        if (sum === win) {
+          r_win++;
+        } else if (sum === (-1 * win)) {
+          y_win++;
+        }
+      }
+    }
+    return  {'r':r_win, 'y':y_win};
+  }
+
+  function check_for_win() {
+    row_win = check_rows();
+    col_win = check_columns();
+    diag_up_win = {'r': 0, 'y': 0}; // check_diagonal_up(); //TODO
+    diag_dwn_win = {'r': 0, 'y': 0}; // check_diagonal_down(); //TODO
+    r_wins = row_win['r'] + col_win['r'] + diag_up_win['r'] + diag_dwn_win['r'];
+    y_wins = row_win['y'] + col_win['y'] + diag_up_win['y'] + diag_dwn_win['y'];
+    if (r_wins > y_wins) {
+      alert('Red wins!');
+      // reset();    
+    } else if  (r_wins < y_wins) {
+      alert('Yellow wins!');
+      // reset();    
+    }
+  }
+
+// BOARD UTILS ////////////////////////////////////////////////////////
+
+  // Group end of turn functions together
+  function end_turn() {
+    swap_player();
+    // TODO: CHECK FOR WIN
+    draw_board();
+    check_for_win(); 
+  }
+
+  // Draw board
+  function draw_board() {
+    for (i = 0; i < size; i++) {
+      for (j = 0; j < size; j++){
+        space = "#sq_" + i + "_" +  j;
+        if (j < board[i].length) {
+          value = board[i][j];
+          if (value === 1) {
+            // $(space).attr('class', 'square red');
+            // $(space).addClass('red');
+            $(space).css('background-color', 'red');
+          } else if (value === -1) {
+            // $(space).attr('class', 'square yellow');
+            $(space).css('background-color', 'yellow');
+          }          
+        } else {
+          // Removes colors that are no longer spaces
+          $(space).css('background-color', 'lightgrey');
+        }
+      }
+    }
+  }
+
+  // Swap player
+  function swap_player() {
+    current_player *= -1;
+    $("#player_square").removeClass();
+    if (current_player === 1){
+      $("#player_square").css('background-color', 'red');;
+    } else {
+      $("#player_square").css('background-color', 'yellow');;
+    }
+  }
+
+  // Draw win (TODO)
+
+
+  function get_empty_board(size){
+    var temp = [];
+    for (i = 0; i < size; i++) {
+      temp.push([]);
+    }
+    return temp;
+  }
+
+});
